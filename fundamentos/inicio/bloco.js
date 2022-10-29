058be4a9f554999ac6a8dbe53c2302d675a8dd49
josefabiosan <josefabiosan@gmail.com>
2022-10-28 21:43:04 -0300

    criando sentenca

---
+++ fundamentos/inicio/bloco.js
@@ -0,0 +1,18 @@
+{
+    console.log("passo #1")
+    console.log("passo #2")
+}
+
+{
+    //sentenca vazia
+    ;
+    ;
+
+}
+{
+    {
+        {
+            //bloco de codigo dentro de bloco
+        }
+    }
+}
