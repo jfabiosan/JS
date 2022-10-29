3c536e472b288319933ffc7a75b72a5fba9f3a04
josefabiosan <josefabiosan@gmail.com>
2022-10-28 21:19:39 -0300

    inserindo comentario no algoritmo

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,13 @@
+//duas barras insere comentario no algoritmo
+console.log("imperativa passo 1");
+//console.log exibe mensagem no terminal
+console.log("imperativa passo 2") //comentario na mesma linha do codigo
+//comentario abaixo para desativar a linha do algoritmo
+//console.log("imperativa passo 2");
+
+/*
+* comentario
+* de
+* multiplas
+* linhas
+*/
