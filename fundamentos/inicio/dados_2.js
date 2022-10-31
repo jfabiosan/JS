2bcf4387a9057e1bbfd61d52c2bb8c9e19216b0d
josefabiosan <josefabiosan@gmail.com>
2022-10-30 21:34:31 -0300

    concatenacao

--- fundamentos/inicio/dados_2.js
+++ fundamentos/inicio/dados_2.js
@@ -4,3 +4,10 @@ let precoComDesconto = preco * (1-desconto);
 
 console.log(precoComDesconto);
 
+let nome = "Caderno";
+let categoria = "Papelaria";
+
+console.log("Produto: " + nome
+    + ", Categoria: " + categoria
+    + ", Preço: " + preco
+    + ", Desconto: " + desconto);
