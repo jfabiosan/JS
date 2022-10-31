9ddb435e8ea89ea1eff43a30592cf6e427044abb
josefabiosan <josefabiosan@gmail.com>
2022-10-30 21:47:53 -0300

    typeof tipo de variavel

--- fundamentos/inicio/dados_3.js
+++ fundamentos/inicio/dados_3.js
@@ -2,3 +2,9 @@ let idade = 31;
 
 console.log(typeof 31);
 console.log(typeof idade);
+
+/*JS considera todo numero como number, diferente de outras
+linguagens que considera double */
+
+let salario = 31.570
+console.log(typeof salario);
