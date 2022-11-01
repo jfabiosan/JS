8af5d012d8957646f39483f6c9d2e73a866aeda6
josefabiosan <josefabiosan@gmail.com>
2022-10-31 23:50:45 -0300

    usando let e const

---
+++ fundamentos/inicio/desafio_area.js
@@ -0,0 +1,8 @@
+const PI = 3.1414;
+const raio = 10;
+let area = PI * raio * raio;
+
+console.log("O resultado da area é: "+ area + "m².");
+
+area = Math.PI * raio * raio;
+console.log("O resultado da area é: "+ area + "m².");
