de68588d1a44cf2edfc6c9ba1c5c6bd059197176
josefabiosan <josefabiosan@gmail.com>
2022-11-04 23:38:53 -0300

    desafio de concatenacao e atribuicao

---
+++ fundamentos/operadores/desafio_atribuicao.js
@@ -0,0 +1,8 @@
+let lista = "Lista de Aprovados";
+lista += "\n==================";
+lista += "\n\n1. Ana Silva";
+lista += "\n2. Pedro Albuquerque";
+lista += "\n3. Guilherme pereira";
+lista += "\n4. Rebeca França";
+
+console.log(lista);
