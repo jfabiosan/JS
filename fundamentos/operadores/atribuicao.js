a8102faf96ecd83f7c9b14e116eded48295821e8
josefabiosan <josefabiosan@gmail.com>
2022-11-04 23:09:35 -0300

    atribuicao de valores

---
+++ fundamentos/operadores/atribuicao.js
@@ -0,0 +1,20 @@
+let x = 10;
+x = x + 6;
+x = x - 1;
+
+console.log(x);
+
+let y = 10;
+y += 20; //y = y + 20
+y -= 2;  //y = y - 20
+y *= 5;  //y = y * 20
+y /= 4;  //y = y / 20
+y %= 2;  //y = y % 20
+
+console.log(y);
+
+let w = "Meu";
+w += " texto"
+w += " aqui."
+
+console.log(w);
