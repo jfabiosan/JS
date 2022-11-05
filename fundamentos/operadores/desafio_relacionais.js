e1d374569e6dea08e95f361f83522ec1b5b019f0
josefabiosan <josefabiosan@gmail.com>
2022-11-04 22:30:54 -0300

    desafio de operadores relacionais

---
+++ fundamentos/operadores/desafio_relacionais.js
@@ -0,0 +1,17 @@
+let a = 1;
+let b = 2;
+
+//5 expressoes TRUE
+console.log(a < b);
+console.log(a <= b);
+console.log(b > a);
+console.log(a != b);
+console.log(b >= a);
+
+//5 expressoes FALSE
+
+console.log();
+console.log();
+console.log();
+console.log();
+console.log();
