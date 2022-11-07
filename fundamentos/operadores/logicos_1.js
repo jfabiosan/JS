79d84152adb660c4d9353798abadda0b89fca31f
josefabiosan <josefabiosan@gmail.com>
2022-11-06 22:01:17 -0300

    operador logico OR

--- fundamentos/operadores/logicos_1.js
+++ fundamentos/operadores/logicos_1.js
@@ -1,6 +1,14 @@
 //Operador Logico AND (&&)
 let temDinheiro = false;
 let estaEnsolarado = true;
-
+//para ser verdadeiro os dois precisam ser verdadeiros
 let resultadoAND = temDinheiro && estaEnsolarado;
 console.log("Vai para o shopping? ="+ resultadoAND);
+
+
+//Operador Logico OR (||)
+let saldoDebito = true;
+let saldoCredito = true;
+//para ser verdadeiro somente um ou os dois precisam ser verdadeiros
+let resultadoOR = saldoDebito || saldoCredito;
+console.log("Vou comprar no mercado? =" +resultadoOR);
