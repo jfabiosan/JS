430299e1d32dd32c068feb5292165134fe514f6e
josefabiosan <josefabiosan@gmail.com>
2022-11-06 22:25:02 -0300

    operador XOR

--- fundamentos/operadores/logicos_1.js
+++ fundamentos/operadores/logicos_1.js
@@ -1,7 +1,7 @@
 //Operador Logico AND (&&)
-let temDinheiro = false;
+let temDinheiro = true;
 let estaEnsolarado = true;
-//para ser verdadeiro os dois precisam ser verdadeiros
+//para ser verdadeiro os dois precisam ser verdadeiros pois só com dinheiro e com sol pra sair ao shopping
 let resultadoAND = temDinheiro && estaEnsolarado;
 console.log("Vai para o shopping? ="+ resultadoAND);
 
@@ -9,6 +9,17 @@ console.log("Vai para o shopping? ="+ resultadoAND);
 //Operador Logico OR (||)
 let saldoDebito = true;
 let saldoCredito = true;
-//para ser verdadeiro somente um ou os dois precisam ser verdadeiros
+/*para ser verdadeiro somente um ou os dois precisam ser verdadeiros pois so com saldo em alguma modalidade
+para comprar */
 let resultadoOR = saldoDebito || saldoCredito;
 console.log("Vou comprar no mercado? =" +resultadoOR);
+
+//Operador Logico XOR (^)bit a bit (!=) booleano exemplo:
+console.log("XOR " +(true ^ false));
+console.log("XOR " +(true != false));
+
+let carro = true;
+let moto = true;
+//para ser verdadeiro somente um pode ser verdadeiros pois nao consegue pilotar os dois ao mesmo tempo.
+let resultadoXOR = carro != moto;
+console.log("XOR Vou sair em um veiculo? " +resultadoXOR);
