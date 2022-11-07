4aab21310556d3464d8ac2ed5afa9a047c43b52a
josefabiosan <josefabiosan@gmail.com>
2022-11-06 22:52:13 -0300

    organizando quebra de linhas

--- fundamentos/operadores/logicos_1.js
+++ fundamentos/operadores/logicos_1.js
@@ -3,7 +3,7 @@ let temDinheiro = true;
 let estaEnsolarado = true;
 //para ser verdadeiro os dois precisam ser verdadeiros pois só com dinheiro e com sol pra sair ao shopping
 let resultadoAND = temDinheiro && estaEnsolarado;
-console.log("Vai para o shopping? "+ resultadoAND);
+console.log("AND Vai para o shopping? "+ resultadoAND);
 
 
 //Operador Logico OR (||)
@@ -12,10 +12,10 @@ let saldoCredito = true;
 /*para ser verdadeiro somente um ou os dois precisam ser verdadeiros pois so com saldo em alguma modalidade
 para comprar */
 let resultadoOR = saldoDebito || saldoCredito;
-console.log("Vou comprar no mercado? " +resultadoOR);
+console.log("\nOR Vou comprar no mercado? " +resultadoOR);
 
 //Operador Logico XOR (^)bit a bit (!=) booleano exemplo:
-console.log("XOR " +(true ^ false));
+console.log("\nXOR " +(true ^ false));
 console.log("XOR " +(true != false));
 
 let carro = false;
@@ -25,5 +25,9 @@ let resultadoXOR = carro != moto;
 console.log("XOR Consigo pilotar? " +resultadoXOR);
 
 //Operador Logico NOT
-console.log(!true);
-console.log(!false);
+console.log('\nResultado não verdadeiro: ' +!true);
+console.log('Resultado não falso: ' +!false);
+
+console.log("\nDiferença do AND para o relacional igual");
+console.log('Resultado verdadeiro para o relacional de igualdade: ' +(false == false));
+console.log('Resultado falso para (AND): ' +(false && false));
