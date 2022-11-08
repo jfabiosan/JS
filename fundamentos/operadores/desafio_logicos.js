32314ccb08cd1a1728077050cf2a5053a6934238
josefabiosan <josefabiosan@gmail.com>
2022-11-07 23:14:04 -0300

    exercicio com operadores

---
+++ fundamentos/operadores/desafio_logicos.js
@@ -0,0 +1,14 @@
+//Duas expressoes TRUE usando (aritmeticos/Relacionais/logicos)
+let vdd1 = (7=="7")&&(7>4);
+let vdd2 = (10-10)==0 || 5 > 0;
+
+console.log(vdd1);
+console.log(vdd2);
+
+
+//Duas expressoes TRUE usando (aritmeticos/Relacionais/logicos)
+let false1 = 10 > 20 && 20 <= 10;
+let false2 = ("Maria" === "joao") && (10 > 15) || ("maria" != "maria");
+
+console.log (false1);
+console.log (false2);
