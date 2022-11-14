9ebc7904d44f637999c54689e96259823459efb0
josefabiosan <josefabiosan@gmail.com>
2022-11-13 21:53:21 -0300

    teste da dupla negacao

---
+++ fundamentos/operadores/unarios_3.js
@@ -0,0 +1,11 @@
+// teste da dupla negacao
+console.log(!!"texto"); // texto 
+console.log(!!""); // texto vazio
+
+console.log(!!0 ); // valor zero false
+console.log(!!1 ); // valor 1 true
+console.log(!!0.1 ); // qualquer valor diferente de zero true
+console.log(!!-1 ); // qualquer valor diferente de zero true
+
+
+
