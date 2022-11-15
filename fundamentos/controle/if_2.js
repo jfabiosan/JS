ba9c4a2ddf7528eeea99fb65bf23cf6fd4603f24
josefabiosan <josefabiosan@gmail.com>
2022-11-15 20:14:47 -0300

    if

--- fundamentos/controle/if_2.js
+++ fundamentos/controle/if_2.js
@@ -2,8 +2,14 @@ const nota = 9;
 const bomComportamento = false;
 
 if (nota >= 7 && bomComportamento)
+    console.log("Parabens, continue assim!");
+
+if (nota >= 7 || bomComportamento)
     console.log("Parabens!");
-else 
-console.log("reprovado");
+
+if (nota <=7 || bomComportamento)
+    console.log("Parabens pelo Comportamento!");
+    //else 
+//console.log("reprovado");
 //if (nota < 7)
   //  console.log("Reprovado");
