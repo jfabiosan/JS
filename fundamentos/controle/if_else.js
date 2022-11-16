6595c24ac77143a4062c210241634f7d859e9723
josefabiosan <josefabiosan@gmail.com>
2022-11-15 21:17:11 -0300

    teórico if else

---
+++ fundamentos/controle/if_else.js
@@ -0,0 +1,9 @@
+if (true)
+    console.log("Exec... if true!");
+else
+    console.log("Nao exec... if true");
+
+if (false)
+    console.log("Nao exec... if false");
+else    
+    console.log("Exec... else do if false!");
