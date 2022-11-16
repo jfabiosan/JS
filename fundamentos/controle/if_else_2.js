a65cd706ecac17a63e72401f23c8b398d98634f2
josefabiosan <josefabiosan@gmail.com>
2022-11-15 21:43:01 -0300

    exercicio 2 if else

--- fundamentos/controle/if_else_2.js
+++ fundamentos/controle/if_else_2.js
@@ -1,9 +1,13 @@
-const hora = 9;
-
+const hora = 18.30;
 let saudacao;
 
 if (hora < 12){
-    console.log(saudacao = 'Bom dia');
+    saudacao = 'Bom dia';
+} else {
+    saudacao = "Boa tarde";
 }
-else
-    console.log("Boa tarde");
+if (hora >= 18){
+    saudacao = 'Boa noite';
+}
+
+console.log(saudacao);
