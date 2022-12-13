b6f9f7212b76017c142856ad941e731f03264206
josefabiosan <josefabiosan@gmail.com>
2022-12-12 23:43:55 -0300

    treinando FOR

---
+++ fundamentos/controle/for_1.js
@@ -0,0 +1,9 @@
+/*for (declaracao da variavel; expressao; incremento) {
+
+    comandos
+} */
+for (let controle = 1; controle <=10; controle +=2){
+
+    console.log(controle);
+}
+console.log("FIM!");
---
+++ fundamentos/controle/for_2.js
@@ -0,0 +1,10 @@
+//usando for com parametros separados
+
+let controle = 1
+
+for (;controle <= 10;){
+
+    console.log (controle);
+    controle +=2;
+}
+console.log ("Fim!");
