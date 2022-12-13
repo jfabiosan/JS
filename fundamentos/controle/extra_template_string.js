045c51bb6f16fbf5e9a3b50f2eaef309ef7f8151
josefabiosan <josefabiosan@gmail.com>
2022-12-13 00:29:17 -0300

    concatenar string com template string no JS

---
+++ fundamentos/controle/extra_template_string.js
@@ -0,0 +1,10 @@
+//crase  = template String ``
+
+const i = 2;
+const j = 4;
+//concatenando sem template string
+console.log("i = "+i + ", j = "+j);
+console.log('i = '+i + ', j = '+j);
+
+//concatenando com template sting
+console.log(`i = ${i}`+`, j = ${j}`);
