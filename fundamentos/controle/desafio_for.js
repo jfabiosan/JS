bbae294dbb4529a610132a82520567d11e9d3b18
josefabiosan <josefabiosan@gmail.com>
2022-12-18 22:39:36 -0300

    desafio usando for

---
+++ fundamentos/controle/desafio_for.js
@@ -0,0 +1,15 @@
+/*desafio usando valores numericos para controlar o laço
+
+let sharp = '';
+for(i=1; i<=6; i++){
+    sharp += '#'
+    console.log(sharp);
+
+}
+*/
+
+//usando valores booleanos
+
+for ( s = "#"; s!= "#######"; s += "#"){
+    console.log(s);
+}
