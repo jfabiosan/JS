1c831ccf299dcb43f4ab12ab80e88f9ef51badd5
josefabiosan <josefabiosan@gmail.com>
2023-01-15 21:41:38 -0300

    exercicios com funcao

---
+++ fundamentos/funcoes/desafio_funcao_1.js
@@ -0,0 +1,15 @@
+const num1 = 3
+const num2 = 4
+
+function soma(n1, n2){
+    console.log(n1 + n2);
+}
+soma(num1, num2);
+
+const num3 = 8
+const num4 = 6
+
+function subtracao(n3, n4){
+    console.log(n3 - n4);
+}
+subtracao(num3, num4);
