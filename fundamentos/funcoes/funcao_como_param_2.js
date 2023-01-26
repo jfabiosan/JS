3e7330c3466641dbef24c88b7650b6738505b281
josefabiosan <josefabiosan@gmail.com>
2023-01-25 23:27:46 -0300

    funcao com valor padrao

--- fundamentos/funcoes/funcao_como_param_2.js
+++ fundamentos/funcoes/funcao_como_param_2.js
@@ -1,11 +1,24 @@
-function executar(fn){
+function executar(fn,n1=10,n2=20){
     if(typeof fn === "function"){
-        console.log(fn(90,50));
+        console.log(fn(n1,n2));
     }
 }
 
 function soma(a,b){
     return a+b;
 }
+function subtrair(a,b){
+    return a-b;
+}
+function multiplica(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
 
+executar(soma,10,20);
 executar(soma);
+executar(subtrair,10,20);
+executar(multiplica,10,20);
+executar(divide,10,20);
