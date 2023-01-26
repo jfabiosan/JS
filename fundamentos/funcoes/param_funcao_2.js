9ca0f1df14027174824321d1ddccdc57dd282043
josefabiosan <josefabiosan@gmail.com>
2023-01-25 22:51:20 -0300

    parametros de funcao- usando valor padrao

---
+++ fundamentos/funcoes/param_funcao_2.js
@@ -0,0 +1,12 @@
+function somar(a=0,b=0,c=0,d=0){
+    return a+b+c+d;
+}
+console.log(somar(1,3,4,6));
+console.log(somar(1,3,4));
+console.log(somar(1,3));
+console.log(somar(1));
+console.log(somar());
+
+
+let x = somar(1,3,4,6);
+console.log(x);
