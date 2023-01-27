3dadaf9667366e7b11fe678a94355d3cb8fb0f8a
josefabiosan <josefabiosan@gmail.com>
2023-01-26 23:03:19 -0300

    chamada da funcao dentro da funcao

--- fundamentos/funcoes/retornar_funcao_1.js
+++ fundamentos/funcoes/retornar_funcao_1.js
@@ -8,4 +8,18 @@ function retornar() {
 }
 console.log(retornar);
 console.log(retornar());
+console.log(retornar()());
 
+
+const umaFuncao = retornar();
+console.log(umaFuncao());
+
+function back() {
+
+    function text(){
+        return "Hello Mr. Lee!!";
+    }
+
+    return text();
+}
+console.log(back());
