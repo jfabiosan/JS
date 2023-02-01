29045fba539309187ebaf9ad75e875c3ada5c161
josefabiosan <josefabiosan@gmail.com>
2023-01-31 23:44:51 -0300

    percorrendo array com for

---
+++ fundamentos/arrays/desafio_for.js
@@ -0,0 +1,11 @@
+const numeros = [1,2,3,4,5,6,7,8,9,10,15,25];
+
+//let valores='';
+//for (n of numeros){
+ //   valores +=n +' ';
+//}
+//console.log(valores);
+
+for(let i = numeros.length -2; i >= 0; i-=2){
+    console.log(numeros[i]);
+}
