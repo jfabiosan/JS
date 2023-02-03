f1c7e99754e87671b1413257cfb032abcf8695d9
josefabiosan <josefabiosan@gmail.com>
2023-02-02 22:49:01 -0300

    metodos do array

---
+++ fundamentos/arrays/metodos_1.js
@@ -0,0 +1,13 @@
+const numeros = [1,2,3,4,5];
+
+numeros[0] = 10;
+numeros[4] = 50;
+numeros.push(6);
+numeros[5] = 60;
+
+console.log(numeros.indexOf(4));
+console.log(numeros.indexOf(3, 2));
+console.log(numeros.includes(10));
+console.log(numeros.join(' - '));
+console.log(numeros.join(' '));
+console.log(numeros);
