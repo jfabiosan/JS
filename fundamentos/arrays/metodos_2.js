97e1fd827feb72892c26fe383698d4cda28ac077
josefabiosan <josefabiosan@gmail.com>
2023-02-02 23:01:09 -0300

    splice e pop no array

---
+++ fundamentos/arrays/metodos_2.js
@@ -0,0 +1,13 @@
+const numeros = [1,2,3,4,5,8];
+
+//SPLICE remove apartir do indice 1, dois elementos
+numeros.splice(1, 2);
+console.log(numeros);
+
+//POP remove o ultimo elemento
+console.log(numeros.pop());
+
+//POP remove novamente o ultimo elemento
+console.log(numeros.pop());
+//exibe na tela o resultado apos remocao dos elementos do array
+console.log(numeros);
