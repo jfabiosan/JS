d6416aa2b250cd64ebbe9e526fe7bfa5d9399443
josefabiosan <josefabiosan@gmail.com>
2023-02-03 00:32:06 -0300

    forEach

---
+++ fundamentos/arrays/metodos_3.js
@@ -0,0 +1,11 @@
+const numeros = [1,2,3,4,5,6,7,8];
+
+for(let n of numeros){
+    console.log(n);
+}
+
+/*for (let i = 0; i < numeros.length; i++) {
+    const element = numeros[i];
+    console.log(element);
+}*/
+
