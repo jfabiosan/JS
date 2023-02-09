af835396ae2424bf3f0da2f349eb7c945d89dbd5
josefabiosan <josefabiosan@gmail.com>
2023-02-08 23:06:34 -0300

    aletrando atributos de um objeto

--- fundamentos/objetos/basic_3.js
+++ fundamentos/objetos/basic_3.js
@@ -4,7 +4,12 @@ const data = {
     ano: 2023,
 };
 
+
+
 console.log(data.dia);
 console.log(data.mes);
 console.log(data.ano);
+
+data.dia = 27;
+data.mes = 12;
 console.log(`${data.dia}/${data.mes}/${data.ano}`);
