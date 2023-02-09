b2f84363b36ed60e50f1d6b8f7c431f8fd296b32
josefabiosan <josefabiosan@gmail.com>
2023-02-08 23:35:50 -0300

    objeto com funcao

---
+++ fundamentos/objetos/desafio_objeto_1.js
@@ -0,0 +1,10 @@
+const nascimento = {
+    dia: 1,
+    mes: 02,
+    ano: 2023,
+    exibirData: function(){
+        return `${this.dia}/${this.mes}/${this.ano}`;
+}
+};
+
+console.log(nascimento.exibirData());
