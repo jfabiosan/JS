e22ca7aecb0578f3e6dd110ab051c8501e043646
josefabiosan <josefabiosan@gmail.com>
2023-02-08 22:48:02 -0300

    POO

--- fundamentos/arrays/metodos_1.js
+++ fundamentos/arrays/metodos_1.js
@@ -1,8 +1,9 @@
 const numeros = [1,2,3,4,5];
 
-numeros[0] = 10;
+numeros[0] = 100;
 numeros[4] = 50;
 numeros.push(6);
+const numeros2 = numeros.concat(7,8,9);
 numeros[5] = 60;
 
 console.log(numeros.indexOf(4));
@@ -11,3 +12,4 @@ console.log(numeros.includes(10));
 console.log(numeros.join(' - '));
 console.log(numeros.join(' '));
 console.log(numeros);
+console.log(numeros2);
