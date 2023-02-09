75d614676b12de6b170c35f77ea4a74595094557
josefabiosan <josefabiosan@gmail.com>
2023-02-08 22:47:33 -0300

    objetos teoria basica

---
+++ fundamentos/objetos/basic_2.js
@@ -0,0 +1,12 @@
+const produto = {
+    nome: 'ipad',
+    preco: 5600,
+    desconto: 0.5,
+    precoComDesconto: function(){
+        return this.preco * (1 - this.desconto);
+    }
+
+};
+
+console.log(produto.nome);
+console.log(produto.precoComDesconto());
