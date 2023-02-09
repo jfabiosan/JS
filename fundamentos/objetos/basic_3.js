3617da1cd33bd2da5946fc0127d0dde06515b36a
josefabiosan <josefabiosan@gmail.com>
2023-02-08 23:04:04 -0300

    estudando conceitos de objeto

---
+++ fundamentos/objetos/basic_3.js
@@ -0,0 +1,10 @@
+const data = {
+    dia: 1,
+    mes: 10,
+    ano: 2023,
+};
+
+console.log(data.dia);
+console.log(data.mes);
+console.log(data.ano);
+console.log(`${data.dia}/${data.mes}/${data.ano}`);
