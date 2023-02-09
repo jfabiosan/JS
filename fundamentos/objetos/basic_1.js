56dbad32f4e4fa98295c0df51d6f9c9d577f82eb
josefabiosan <josefabiosan@gmail.com>
2023-02-08 22:55:59 -0300

    introducao a POO

---
+++ fundamentos/objetos/basic_1.js
@@ -0,0 +1,10 @@
+const produto = {
+    nome: 'caneta',
+    cor: 'azul',
+    marca: 'bic',
+    preco: 1.50,
+};
+
+console.log(produto.marca);
+console.log(produto.preco);
+console.log(produto.cor);
