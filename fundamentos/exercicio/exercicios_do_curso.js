fc5d376acd05b4ad0bff14113d72744780234881
Fabio <josefabiosan@gmail.com>
2023-02-19 20:52:21 -0300

    exercicios de comparacao e funcao

--- fundamentos/exercicio/exercicios_do_curso.js
+++ fundamentos/exercicio/exercicios_do_curso.js
@@ -71,3 +71,16 @@ function nomeDoMes (mes){
     } 
 }
 console.log(nomeDoMes(12));
+
+/*Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
+Exemplos:
+maiorOuIgual(0, 0) // retornará true
+maiorOuIgual(0, "0") // retornará false
+maiorOuIgual(5, 1) // retornará true
+*/
+function maiorOuIgual (n1, n2){
+    return n1 > n2 || n1 === n2;
+}
+console.log(maiorOuIgual(0, 0));
+console.log(maiorOuIgual(0, "0"));
+console.log(maiorOuIgual(5, 1));
