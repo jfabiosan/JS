9739c7d012c41851b7951a67b893be96998b054f
Fabio <josefabiosan@gmail.com>
2023-02-19 19:53:42 -0300

    exericio para praticar 2

--- fundamentos/exercicio/exercicios_do_curso.js
+++ fundamentos/exercicio/exercicios_do_curso.js
@@ -8,3 +8,17 @@ function saudacao (n){
     console.log(`Olá, ${n}`);
 }
 saudacao('Leonardo!');
+
+/*Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+Obs: considere que um ano tem 365 dias. Desconsidere anos bissextos (com 366 dias) e
+desconsidere também dias decorridos desde o último aniversário.
+Exemplos:
+converterIdadeEmAnosParaDias(25) // retornará 9125
+converterIdadeEmAnosParaDias(70) // retornará 25550
+*/
+
+function convertIdade (idade){
+    return idade * 365;
+}
+console.log(convertIdade(25));
+console.log(convertIdade(70));
