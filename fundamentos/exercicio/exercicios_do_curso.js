08348980013fe643d9c25ee1c68f03424a6cd05b
Fabio <josefabiosan@gmail.com>
2023-02-19 19:43:38 -0300

    exericio para praticar

--- fundamentos/exercicio/exercicios_do_curso.js
+++ fundamentos/exercicio/exercicios_do_curso.js
@@ -1,4 +1,10 @@
+/*Crie uma função que retorna a string "Olá, " concatenada com um argumento text (a ser passado para a
+    função) e com ponto de exclamação "!" no final.
+    Exemplos:
+    cumprimentar("Leonardo") // retornará "Olá, Leonardo!"
+    cumprimentar("Maria") // retornará "Olá, Maria!"
+*/
 function saudacao (n){
-    console.log(`olá,${n}`);
+    console.log(`Olá, ${n}`);
 }
 saudacao('Leonardo!');
