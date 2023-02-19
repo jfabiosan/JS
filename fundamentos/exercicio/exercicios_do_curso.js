39a2fec00fc61508f6a41d37010ad3792032ef17
Fabio <josefabiosan@gmail.com>
2023-02-19 20:41:02 -0300

    exercicio com switch

--- fundamentos/exercicio/exercicios_do_curso.js
+++ fundamentos/exercicio/exercicios_do_curso.js
@@ -4,10 +4,10 @@
     cumprimentar("Leonardo") // retornará "Olá, Leonardo!"
     cumprimentar("Maria") // retornará "Olá, Maria!"
 */
-function saudacao (n){
-    console.log(`Olá, ${n}`);
+function cumprimentar (nome){
+    console.log(`Olá, ${nome}`);
 }
-saudacao('Leonardo!');
+cumprimentar('Leonardo!');
 
 /*Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
 Obs: considere que um ano tem 365 dias. Desconsidere anos bissextos (com 366 dias) e
@@ -18,7 +18,8 @@ converterIdadeEmAnosParaDias(70) // retornará 25550
 */
 
 function convertIdade (idade){
-    return idade * 365;
+    const diasAno = 365;
+    return idade * diasAno;
 }
 console.log(convertIdade(25));
 console.log(convertIdade(70));
@@ -33,3 +34,40 @@ function calculaSalario (horas, valorHora){
     return horas * valorHora;
 }
 console.log(`"Salario igual a R$ ${calculaSalario(150, 40.5)}"`);
+
+/*Crie uma função que recebe um número (de 1 a 12  e retorne o mês correspondente como uma string. Por
+    exemplo, se a entrada for 2, a função deverá retornar "fevereiro", pois este é o 2° mês.
+    Exemplos:
+    nomeDoMes(1) // retornará "janeiro"
+    nomeDoMes(4) // retornará "abril"
+*/
+function nomeDoMes (mes){
+    switch (mes){
+        case 1:
+            return "Janeiro";
+        case 2:
+            return "Fevereiro"
+        case 3:
+            return "Março"
+        case 4:
+            return "Abril"
+        case 5:
+            return "maio"
+        case 6:
+            return "Junho"
+        case 7:
+            return "Julho"
+        case 8:
+            return "Agosto"
+        case 9:
+            return "Setembro"
+        case 10:
+            return "Outubro"
+        case 11:
+            return "Novembro"
+        case 12:
+            return "Dezembro"
+            
+    } 
+}
+console.log(nomeDoMes(12));
